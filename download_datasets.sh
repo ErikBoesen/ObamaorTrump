04e827d97dbc5b8a98a2b5770b24db6f0ff53d56
ErikBoesen <me@erikboesen.com>
2018-11-24 19:28:50 -0500

    Move download_datasets.sh out of data dir

---
+++ download_datasets.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/samim23/obama-rnn/master/input.txt -o data/obama.txt
+curl https://raw.githubusercontent.com/ryanmcdermott/trump-speeches/master/speeches.txt -o data/trump.txt
